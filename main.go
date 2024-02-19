a1e3da49ff8dffd844fc86356cb36f3e9ca928ea
pguma <p.guma.service+git@gmail.com>
2024-02-19 17:43:51 +0900

    add simple database/sql sample

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Printf("%s", "Hello Golang")
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=15432 user=postgres password=password dbname=dev sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT * FROM sample_table WHERE id < $1`, 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int8
+		var name string
+		var age int8
+		var assets int64
+		var height float32
+		var alive bool
+		var birthday string
+		var birthAt string
+		var note string
+
+		err = rows.Scan(&id, &name, &age, &assets, &height, &alive, &birthday, &birthAt, &note)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v \n", id, name, age, assets, height, alive, birthday, birthAt, note)
+	}
 }
